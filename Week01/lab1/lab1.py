6fddcbc07f909bf6d29d462235a3109f76011048
cms-nno <carlosmsdcv@gmail.com>
2026-01-16 14:54:28 -0500

    add q1,q2,q3,q4,q5

---
+++ Week01/lab1/lab1.py
@@ -0,0 +1,30 @@
+# Sample Coding Question 01 Week 01
+# Carlos Macdonald-Simas
+
+
+# Question 2: Defining an array (List)
+my_array = [1, 4, 7 ,9]
+
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Original expression:
+# e = a - b ** c // d + a % c
+
+# Fully-Bracketed Version:
+e = (a - ((b ** c) // d)) + (a % c)
+
+
+# Question 4: Formatting
+temperature = 32.6
+
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Question 5: Common Functions - User Input
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22
+
+print("Now showing the shop items filtered by age:", userAge)
